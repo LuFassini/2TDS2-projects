f0290ec80263c2da24034b2f1d792f70352a1a89
Luana Fassini <luanafassini.564@gmail.com>
2023-08-17 10:16:46 -0300

    feat: add class product

--- js/script.category-product.js
+++ js/script.category-product.js
@@ -1,7 +1,18 @@
+//
 class category {
     constructor (id,name){
         this.id = id;
         this.name = name;
         this.products = []; //varios produtos
+    }
+}
+
+//Produto pertence a uma categoria => 1 por 1 
+class Product {
+    constructor(id,name,price,category){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
     }
 }
